5acdd23230da3b8f985b151f250009e8974a9774
karan <karankumar46226@gmail.com>
2024-02-20 17:17:17 +0530

    cors problem

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ const app=express();
 dotenv.config({path:"./config/config.env"})
 
 app.use(cors({
-    // origin:process.env.FRONTEND_URL,
+    origin:process.env.FRONTEND_URL,
     methods:["GET","POST","DELETE","PUT"],
-    credentials:true
+    // credentials:true
 }));
 
 app.use(cookieParser())
